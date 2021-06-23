27e2c751cd2639a840ca9fd179bb33cd8bc6e2c9
zziri <jihzang@naver.com>
2021-06-24 01:10:23 +0900

    setup: setup components
    필요한 component 들을 setup하고 구현할 skeleton code 생성

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,59 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <Header></Header>
+        <Input v-on:addTask="addTask"></Input>
+        <List v-bind:propsdata="taskList" @removeTask="removeTask"></List>
+        <Footer v-on:removeAll="clearAll"></Footer>
+    </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Input from './components/Input.vue'
+import List from './components/List.vue'
+import Footer from './components/Footer.vue'
 export default {
-  name: 'App'
+	data() {
+		return {
+			taskList: []
+		}
+	},
+	methods: {
+		addTask(task) {
+			alert("called addTask" + task);
+		},
+		clearAll() {
+			alert("called clearAll");
+		},
+		removeTask(task) {
+			alert("called removeTask");
+		}
+	},
+	components: {
+		'Header': Header,
+		'Input': Input,
+		'List': List,
+		'Footer': Footer
+	},
+	created() {
+		alert("call created");
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        text-align: center;
+        background-color: #F6F6F8;
+    }
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+    button {
+        border-style: groove;
+    }
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+    }
 </style>
